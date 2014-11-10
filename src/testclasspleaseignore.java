2adb3a96a5d76eb648e5e62be49e08b5e18b5b33
jebadirad <ebadirad@email.arizona.edu>
2014-11-10 16:57:45 -0700

    Test Commit
    This is just to test our setup of a class that is committed to git.

---
+++ src/testclasspleaseignore.java
@@ -0,0 +1,10 @@
+
+public class testclasspleaseignore {
+
+	public testclasspleaseignore(){
+		System.out.println("Hello all");
+	}
+	public static void main(String[] args){
+		new testclasspleaseignore();
+	}
+}
