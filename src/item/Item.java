e91a2eccf29010710270b75e6c77de7d69ed3687
ebadirad <ebadirad@harding.cs.arizona.edu>
2014-11-16 16:58:15 -0700

    Changed item enum to set items and stats for items.

--- src/item/Item.java
+++ src/item/Item.java
@@ -1,6 +1,24 @@
 package item;
 
 public enum Item {
-	superitem();
+	//items will have their buffs stored in an array
+	
+	//example superitem will give +1 to hp +2 to attack +3attack range and +4 to moverange
+	//[1,2,3,4]
+	superitem(new int[] {1,2,3,4});
+	
+	private int[] modifiers;
+	
+	
+	private Item(int[] array){
+		this.modifiers = array;
+		
+	}
+	
+	public int[] getModifiers(){
+		return modifiers;
+	}
+	
+	
 	
 }
