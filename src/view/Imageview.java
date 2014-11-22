2217fa5cda08fab13e5c1c4f0b4ecce4c0118566
Kal Cramer <kalcramer@email.arizona.edu>
2014-11-22 11:27:33 -0700

    "Added to ImageView and added some full animation sheets for units.

--- src/view/Imageview.java
+++ src/view/Imageview.java
@@ -13,33 +13,31 @@ import javax.imageio.ImageIO;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import model.Cell;
-import model.GameBoard;
-import model.Terrain;
+import model.*;
 
 public class Imageview extends JPanel
 {
-	private static final long serialVersionUID = -4162356814719738349L;
-private BufferedImage image;
+  private static final long serialVersionUID = -4162356814719738349L;
+  private BufferedImage image;
   private Point pos;
-  private BufferedImage hunterImage, grass, bloodImage, goopImage, slimeImage,
-      slimePitImage, wumpusImage, blackImage; // lol wumpus?? we need to review these variables
   private Cell[][] board;
   private GameBoard theModel;
   private JLabel message;
-private BufferedImage grassTile;
-private BufferedImage itemSheet;
-private BufferedImage cloneTrooper;
-private BufferedImage jediSheet;
+  private BufferedImage grass;
+
+  private BufferedImage grassTile;
+  private BufferedImage itemSheet;
+  private BufferedImage cloneTrooper;
+  private BufferedImage jediSheet;
 
   public Imageview()
   {
     setLayout(null);
-    message = new JLabel("");
+    message = new JLabel("Test");
     message.setFont(new Font("Algerian", Font.BOLD, 32));
-    message.setBounds(0, 0, 500, 500);
+    message.setBounds(0, 0, 800, 800);
     message.setForeground(Color.RED);
-
+    GameBoard g = GUI.gameboard;
     add(message);
     pos = new Point(0, 0);
     try
@@ -51,27 +49,20 @@ private BufferedImage jediSheet;
     {
       e.printStackTrace();
     }
+    this.setVisible(true);
 
   }
-
-   public void update(GameBoard arg0)
-   {
-   theModel = (GameBoard) arg0;
-   board = theModel.getBoard();
-   repaint();
-   }
 
   @Override
   public void paintComponent(Graphics g)
   {
-    
-
     super.paintComponent(g);
     Graphics2D g2 = (Graphics2D) g;
-
     g2.setPaint(Color.WHITE);
     g2.fillRect(0, 0, 1280, 500);
-
+    g2.setPaint(Color.BLACK);
+    g2.setFont(new Font("Courier New", Font.BOLD, 17));
+    int y = 0;
     for (int i = 0; i < 10; i++)
     {
       for (int j = 0; j < 10; j++)
@@ -105,48 +96,67 @@ private BufferedImage jediSheet;
     }
   }
 
-  public void loadImages() {
-		try {
-			grassTile = ImageIO.read(new File("images" + File.separator
-					+ "Grass.png"));
-		} catch (IOException e) {
-			System.out.println("Could not find 'Grass.png'");
-		}
-		try {
-			itemSheet = ImageIO.read(new File("images" + File.separator
-					+ "all_items.png"));
+  public void loadImages()
+  {
+    try
+    {
+      grassTile = ImageIO
+          .read(new File("images" + File.separator + "Grass.png"));
+    }
+    catch (IOException e)
+    {
+      System.out.println("Could not find 'Grass.png'");
+    }
+    try
+    {
+      itemSheet = ImageIO.read(new File("images" + File.separator
+          + "all_items.png"));
 
-		} catch (IOException e) {
-			System.out.println("Could not find 'all_items.png'");
-		}
-		try {
-			cloneTrooper = ImageIO.read(new File("images" + File.separator
-					+ "CloneTrooper.png"));
+    }
+    catch (IOException e)
+    {
+      System.out.println("Could not find 'all_items.png'");
+    }
+    try
+    {
+      cloneTrooper = ImageIO.read(new File("images" + File.separator
+          + "CloneTrooper.png"));
 
-		} catch (IOException e) {
-			System.out.println("Could not find 'CloneTrooper.png'");
-		}
-		try {
-			jediSheet = ImageIO.read(new File("images" + File.separator
-					+ "jedi-spritesheet.png"));
+    }
+    catch (IOException e)
+    {
+      System.out.println("Could not find 'CloneTrooper.png'");
+    }
+    try
+    {
+      jediSheet = ImageIO.read(new File("images" + File.separator
+          + "jedi-spritesheet.png"));
 
-		} catch (IOException e) {
-			System.out.println("Could not find 'jedi-spritesheet.png'");
-		}
-		try {
-			itemSheet = ImageIO.read(new File("images" + File.separator
-					+ "range-tank-spritesheet.png"));
+    }
+    catch (IOException e)
+    {
+      System.out.println("Could not find 'jedi-spritesheet.png'");
+    }
+    try
+    {
+      itemSheet = ImageIO.read(new File("images" + File.separator
+          + "range-tank-spritesheet.png"));
 
-		} catch (IOException e) {
-			System.out.println("Could not find 'range-tank-spritesheet.png'");
-		}
-		try {
-			itemSheet = ImageIO.read(new File("images" + File.separator
-					+ "Medic.png"));
+    }
+    catch (IOException e)
+    {
+      System.out.println("Could not find 'range-tank-spritesheet.png'");
+    }
+    try
+    {
+      itemSheet = ImageIO
+          .read(new File("images" + File.separator + "Medic.png"));
 
-		} catch (IOException e) {
-			System.out.println("Could not find 'Medic.png'");
-		}
+    }
+    catch (IOException e)
+    {
+      System.out.println("Could not find 'Medic.png'");
+    }
   }
 
 }
