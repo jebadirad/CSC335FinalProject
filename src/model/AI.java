3aec3ed408be8d00762d9399ae4525b17c4bec33
ebadirad <ebadirad@harpoon.cs.arizona.edu>
2014-12-07 18:19:09 -0700

    Setup commands for game.

--- src/model/AI.java
+++ src/model/AI.java
@@ -3,15 +3,13 @@ package model;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.Timer;
-
 import view.GUI;
 
 public class AI {
 	public static final int WAIT_TIME = 500;
-
+	
 	public AI() {
-
+		
 	}
 
 	public void makeMove() {
@@ -20,21 +18,16 @@ public class AI {
 		for (int i = 0; i < unitsWithMovesLeft; i++) {
 			GUI.CurrentUnitSelected = GUI.gameboard.getPlayer1Units().get(i);
 			System.out.println(GUI.CurrentUnitSelected.getUnit().toString());
-			Timer t = new Timer(WAIT_TIME, new MoveListener());
-
-			while (GUI.CurrentUnitSelected.getUnit().getMovesLeft() > 0) {
+			
+			
+			while (GUI.gameboard.canMove(GUI.CurrentUnitSelected, "L")) {
+				
 				System.out.println("Before");
-				t.start();
+				GUI.moveLeft.doClick();
+				
 				System.out.println("After");
-				try {
-					t.wait(200);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
+				
 			}
-			t.stop();
 			unitsWithMovesLeft--;
 
 		}
@@ -47,7 +40,7 @@ public class AI {
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			System.out.println("Got here");
-			if (GUI.gameboard.canMove(GUI.CurrentUnitSelected, "L") == false) {
+			if (GUI.gameboard.canMove(GUI.CurrentUnitSelected, "L") == true) {
 				GUI.moveLeft.doClick();
 			} else {
 				GUI.moveRight.doClick();
---
+++ src/model/ActionCommand.java
@@ -0,0 +1,11 @@
+package model;
+
+public class ActionCommand extends Command{
+
+	@Override
+	public void execute(Object executeOn) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
