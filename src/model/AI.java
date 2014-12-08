796440dc53ffe7a67fdd4e64c955e43363616c15
ebadirad <ebadirad@harpoon.cs.arizona.edu>
2014-12-07 20:39:44 -0700

    Finished attack command and move command, needs to be tested.

--- src/model/AI.java
+++ src/model/AI.java
@@ -15,7 +15,10 @@ public class AI {
 				GUI.CurrentUnitSelected = GUI.gameboard.getPlayer1Units().get(i);	
 				ActionCommand moveleft = new ActionCommand("L", GUI.CurrentUnitSelected);
 				ActionCommand moveright = new ActionCommand("R", GUI.CurrentUnitSelected);
-					if(i < 1){
+				AttackCommand attack = new AttackCommand(GUI.CurrentUnitSelected, GUI.EnemyUnitSelected);	
+				GUI.gameboard.commandqueue.add(attack);
+				if(i < 1){
+						
 						GUI.gameboard.commandqueue.add(moveleft);
 					}
 					else{
