90c49c6fdd9d010f2694e38739794aece6b8e67d
ebadirad <ebadirad@harken.cs.arizona.edu>
2014-12-13 18:16:12 -0700

    Fixed player status page!

--- src/view/PlayerStatus.java
+++ src/view/PlayerStatus.java
@@ -36,6 +36,8 @@ public class PlayerStatus extends JPanel {
 		g2.setPaint(Color.BLACK);
 		g2.setFont(new Font("Courier New", Font.BOLD, 17));
 		int y = 100;
+		int x = 110;
+		int counter = 0;
 for(Cell element:units){
 	BufferedImage image = null;
 	try {
@@ -44,19 +46,45 @@ for(Cell element:units){
 		// TODO Auto-generated catch block
 		System.out.println("404:FILE NOT FOUND");
 	}
-	g2.drawImage(image, 0, y-100 , null);
-	g2.drawString(element.getUnit().toString(),110,y-80);
-	g2.drawString("Damage: " + element.getUnit().getDamage() +"  Attack Range: " + element.getUnit().getAttackRange(),110,y-60);
-	g2.drawString("Health: " + element.getUnit().getHealth() + "   Moves Left: " + element.getUnit().getMovesLeft(), 110, y-40);
+	if(counter == 3 ){
+		x = x + 700;
+		y=100;
+	}
+	g2.drawImage(image, x-110, y-100 , null);
+	g2.drawString(element.getUnit().toString(),x,y-80);
+	g2.drawString("Damage: " + element.getUnit().getDamage() +"  Attack Range: " + element.getUnit().getAttackRange(),x,y-60);
+	g2.drawString("Health: " + element.getUnit().getHealth() + "   Moves Left: " + element.getUnit().getMovesLeft(), x, y-40);
 	if(element.getUnit().getCanAttack().equals(true)){
-		g2.drawString("Can Attack: Yes", 110, y-20);
+		g2.drawString("Can Attack: Yes", x, y-20);
 	}
 	else{
-		g2.drawString("Can Attack: No", 110, y-20);
+		g2.drawString("Can Attack: No", x, y-20);
 	}
-	g2.drawString(element.getUnit().getDescription(), 110, y);
+	if(element.getUnit().getDescription().length() >= 45){
+		String desc = element.getUnit().getDescription();
+		String desc1 = desc.substring(0,45);
+		String desc2 = desc.substring(45);
+		String desc3 = "";
+		if(desc2.length() >= 45){
+			desc3 = desc2.substring(45);
+			desc2 = desc2.substring(0,45);
+		}
+		g2.drawString(desc1, x, y);
+		g2.drawString(desc2,x, y+20);
+		if(desc3.equals("")){
+			
+		}
+		else{
+			g2.drawString(desc3, x,y+40);
+		}
+	}
+	else{
+		g2.drawString(element.getUnit().getDescription(), x, y);
+	}
 	
-	y = y + 120;
+	
+	y = y + 160;
+	counter++;
 		}
 	}
 }
